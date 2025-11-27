75e105f48e11c4ab718d958fbed51f58a45ecec4
saddamc <saddam13bd@gmail.com>
2025-11-27 12:38:52 +0600

    Initial commit

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import { NavigationSheet } from "./navigation-sheet";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-6 inset-x-4 h-16 max-w-screen-xl mx-auto rounded-full bg-background border dark:border-slate-700/70 z-30">
+    <nav className="fixed top-6 inset-x-4 h-16 max-w-screen-xl mx-auto bg-background border dark:border-slate-700/70 z-30">
       <div className="flex h-full items-center justify-between px-6 md:px-8">
         {/* Logo with consistent padding */}
         <Link href="/" className="flex-shrink-0 ">
